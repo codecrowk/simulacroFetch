9816dcf778f8c479b99c83df78982ac895759a38
codecrowk <handres41@outlook.com>
2024-02-08 16:01:03 -0500

    Creation of category model and news model

--- js/data_base_model.js
+++ js/data_base_model.js
@@ -1,7 +1,7 @@
 const DATABASE_ROOT_URL = "http://localhost:3000";
 const USERS_ENDPOINT = `${DATABASE_ROOT_URL}/users`;
-const CATEGORIES_ENDPOINT = `${DATABASE_ROOT_URL}/users`  ;
-const NEWS_ENDPOINT = `${DATABASE_ROOT_URL}/users`;
+const CATEGORIES_ENDPOINT = `${DATABASE_ROOT_URL}/categories`  ;
+const NEWS_ENDPOINT = `${DATABASE_ROOT_URL}/news`;
 
 class DataModelBuilder {
   constructor (userEndpoint) {
@@ -44,7 +44,6 @@ class UserDataMoldel extends DataModelBuilder{
   constructor () {
     super(USERS_ENDPOINT)
   }
-
   async post (userName, userEmail, userPassword) {
     const userSchema = {
       name: userName,
@@ -55,5 +54,39 @@ class UserDataMoldel extends DataModelBuilder{
     return response
   }
 }
-const myData = new UserDataMoldel();
-console.log(myData.post("jose Alvarez", "jose123@gmail.com", "123456789"))
+
+// A ramdom writer shouldn't be able to create new categories, just admins 
+class CategoryDataMoldel extends DataModelBuilder{
+  constructor () {
+    super(CATEGORIES_ENDPOINT)
+  }
+  async post (userTitle, userDescription) {
+    const userSchema = {
+      title: userTitle,
+      description: userDescription
+    }
+    const response = await super.post(userSchema);
+    return response
+  }
+}
+
+class NewsDataMoldel extends DataModelBuilder{
+  constructor () {
+    super(NEWS_ENDPOINT)
+  }
+  async post (userTitle, userContent, userCategoryID, userAutorID) {
+    const userSchema = {
+      title: userTitle,
+      content: userContent,
+      category_id: userCategoryID,
+      autor_id: userAutorID
+    }
+    const response = await super.post(userSchema);
+    return response
+  }
+}
+// const myData = new NewsDataMoldel();
+// console.log(myData.post("Two girls start in Json Acdemy", "This storie is about a family how want a better future", "pa23", "d371"))
+
+const myData = new CategoryDataMoldel();
+console.log(myData.post("Sport", "Footbal and others"))
